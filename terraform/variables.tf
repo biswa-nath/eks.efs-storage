eccaf9005b950e62f8861b4fb257c3a94f1898e0
Biswanath <p_biswanath@yahoo.co.in>
2025-09-08 18:00:06 +0530

    variables.tf: Updated default values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,6 @@
-variable "region" {}
+variable "region" {
+   default = "us-east-2"
+}
 variable "cluster_name" {}
 variable "k8s_namespace" {
     default = "kube-system"
@@ -7,7 +9,7 @@ variable "k8s_efs_service_account" {
     default = "efs-csi-controller-sa"
 }
 variable "k8s_efs_sc_name" {
-    default = "efs-sc-dynamic"
+    default = "efs-sc"
 }
 variable "k8s_efs_helm_repo_url" {
     default = "https://kubernetes-sigs.github.io/aws-efs-csi-driver"
